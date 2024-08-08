67a054de5a8a70f88245591133eca4fabdb64f9c
Rama Ayodhya <ramaaydhya@gmail.com>
2024-08-08 11:32:50 +0700

    ch 15 - adding instruction for evaluating arithmetic ops +-*/

--- vm.c
+++ vm.c
@@ -36,6 +36,13 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do                  \
+  {                   \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b);     \
+  } while (false)
 
   for (;;)
   {
@@ -59,6 +66,18 @@ static InterpretResult run()
       Value constant = READ_CONSTANT();
       push(constant);
       break;
+    case OP_ADD:
+      BINARY_OP(+);
+      break;
+    case OP_SUBTRACT:
+      BINARY_OP(-);
+      break;
+    case OP_MULTIPLY:
+      BINARY_OP(*);
+      break;
+    case OP_DIVIDE:
+      BINARY_OP(/);
+      break;
     case OP_NEGATE:
       push(-pop());
       break;
@@ -71,6 +90,7 @@ static InterpretResult run()
 
 #undef READ_CONSTANT
 #undef READ_BYTE
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)
