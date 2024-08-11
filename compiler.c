815dfec7ffc95259c279d4387cc5ee5b3a7b7ccf
Rama Ayodhya <ramaaydhya@gmail.com>
2024-08-11 13:18:01 +0700

    ch 17 - parsing prefix & infix expressions and creating a Pratt parser

--- compiler.c
+++ compiler.c
@@ -13,7 +13,39 @@ typedef struct
   bool panicMode;
 } Parser;
 
+typedef enum
+{
+  // precedence levels, sorted from lowest to highest
+  PREC_NONE,
+  PREC_ASSIGNMENT, // =
+  PREC_OR,         // or
+  PREC_AND,        // and
+  PREC_EQUALITY,   // == !=
+  PREC_COMPARISON, // < > <= >=
+  PREC_TERM,       // + -
+  PREC_FACTOR,     // * /
+  PREC_UNARY,      // ! -
+  PREC_CALL,       // . ()
+  PREC_PRIMARY
+} Precedence;
+
+// ParseFn is a pointer to a function without any parameter that returns void
+typedef void (*ParseFn)();
+
+typedef struct
+{
+  ParseFn prefix;
+  ParseFn infix;
+  Precedence precedence;
+} ParseRule;
+
 Parser parser;
+Chunk *compilingChunk;
+
+static Chunk *currentChunk()
+{
+  return compilingChunk;
+}
 
 static void errorAt(Token *token, const char *message)
 {
@@ -25,7 +57,9 @@ static void errorAt(Token *token, const char *message)
   if (token->type == TOKEN_EOF)
     fprintf(stderr, " at end");
   else if (token->type == TOKEN_ERROR)
-    ; // Nothing happened
+  {
+    ;
+  } // Nothing happened
   else
     fprintf(stderr, " at '%.*s'", token->length, token->start);
 
@@ -66,9 +100,179 @@ static void consume(TokenType type, const char *message)
   errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte)
+{
+  writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2)
+{
+  // store the bytecode for the token type
+  emitByte(byte1);
+  // and the value representation
+  emitByte(byte2);
+}
+
+static void emitReturn()
+{
+  // store the bytecode for `return` token
+  emitByte(OP_RETURN);
+}
+
+static uint8_t makeConstant(Value value)
+{
+  // `constant` is an new last index of the chunk's constant table
+  // after inserting new constant
+  int constant = addConstant(currentChunk(), value);
+  // We limited the constant table's maximum size to 256
+  if (constant > UINT8_MAX)
+  {
+    error("Too many constants in one chunk");
+    return 0;
+  }
+  return (uint8_t)constant;
+}
+
+static void emitConstant(Value value)
+{
+  // store bytecode for the token type and the updated constant table index
+  emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void endCompiler()
+{
+  emitReturn();
+}
+
+static void expression();
+static ParseRule *getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
+
+static void binary()
+{
+  TokenType operatorType = parser.previous.type;
+  ParseRule *rule = getRule(operatorType);
+  parsePrecedence((Precedence)(rule->precedence + 1));
+
+  switch (operatorType)
+  {
+  case TOKEN_PLUS:
+    emitByte(OP_ADD);
+    break;
+  case TOKEN_MINUS:
+    emitByte(OP_SUBTRACT);
+    break;
+  case TOKEN_STAR:
+    emitByte(OP_MULTIPLY);
+    break;
+  case TOKEN_SLASH:
+    emitByte(OP_DIVIDE);
+    break;
+  default:
+    return; // Unreachable;
+  }
+}
+
+static void grouping()
+{
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression");
+}
+
+static void number()
+{
+  // convert the latest consumed token to a double literal
+  double value = strtod(parser.previous.start, NULL);
+  emitConstant(value);
+}
+
+static void unary()
+{
+  TokenType operatorType = parser.previous.type;
+
+  // Compile the operand
+  parsePrecedence(PREC_UNARY);
+
+  // Emit the operator instruction
+  switch (operatorType)
+  {
+  case TOKEN_MINUS:
+    emitByte(OP_NEGATE);
+    break;
+  default:
+    return; // Unreachable;
+  }
+}
+
+// `rules` is a function pointer table that, given a token type, lets us find
+// 1. the func to compile a prefix expr starting with a token of that type,
+// 2. the func to compile an infix expr whose left operand is followed by a token of that type, and
+// 3. the precedence of an infix expression that uses that token as an operator.
+
+// What the heck is this technique?
+// Google "Pratt parser" or "vaughan pratt top down operator precedence parsing"
+ParseRule rules[] = {
+    [TOKEN_LEFT_PAREN] = {grouping, NULL, PREC_NONE},
+    [TOKEN_RIGHT_PAREN] = {NULL, NULL, PREC_NONE},
+    [TOKEN_LEFT_BRACE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_RIGHT_BRACE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_COMMA] = {NULL, NULL, PREC_NONE},
+    [TOKEN_DOT] = {NULL, NULL, PREC_NONE},
+    [TOKEN_MINUS] = {unary, binary, PREC_TERM},
+    [TOKEN_PLUS] = {NULL, binary, PREC_TERM},
+    [TOKEN_SEMICOLON] = {NULL, NULL, PREC_NONE},
+    [TOKEN_SLASH] = {NULL, binary, PREC_FACTOR},
+    [TOKEN_STAR] = {NULL, binary, PREC_FACTOR},
+    [TOKEN_BANG] = {NULL, NULL, PREC_NONE},
+    [TOKEN_BANG_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EQUAL_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_GREATER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_GREATER_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_LESS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_LESS_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_IDENTIFIER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_STRING] = {NULL, NULL, PREC_NONE},
+    [TOKEN_NUMBER] = {number, NULL, PREC_NONE},
+    [TOKEN_AND] = {NULL, NULL, PREC_NONE},
+    [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_ELSE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FALSE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FOR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FUN] = {NULL, NULL, PREC_NONE},
+    [TOKEN_IF] = {NULL, NULL, PREC_NONE},
+    [TOKEN_NIL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_OR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_PRINT] = {NULL, NULL, PREC_NONE},
+    [TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
+    [TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_THIS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_TRUE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_VAR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_WHILE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_ERROR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EOF] = {NULL, NULL, PREC_NONE},
+};
+
+static ParseRule *getRule(TokenType type)
+{
+  return &rules[type];
+}
+
+static void parsePrecedence(Precedence precendence)
+{
+  // Only compile tokens with the specified and higher precedences
+}
+
+static void expression()
+{
+  parsePrecedence(PREC_ASSIGNMENT);
+}
+
 bool compile(const char *source, Chunk *chunk)
 {
   initScanner(source);
+  compilingChunk = chunk;
 
   parser.hadError = false;
   parser.panicMode = false;
@@ -76,5 +280,6 @@ bool compile(const char *source, Chunk *chunk)
   advance();
   expression();
   consume(TOKEN_EOF, "Expect end of expression.");
+  endCompiler();
   return !parser.hadError;
 }
