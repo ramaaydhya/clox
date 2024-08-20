8f13bc03e62f850cbb6c56d668faadb6261481b0
Rama Ayodhya <ramaaydhya@gmail.com>
2024-08-20 19:27:58 +0700

    ch 24

--- compiler.c
+++ compiler.c
@@ -50,20 +50,31 @@ typedef struct
   int depth;
 } Local;
 
-typedef struct
+typedef enum
 {
+  TYPE_FUNCTION,
+  TYPE_SCRIPT,
+} FunctionType;
+
+typedef struct Compiler
+{
+  struct Compiler *enclosing;
+  ObjFunction *function;
+  FunctionType type;
   Local locals[UINT8_COUNT];
   int localCount;
   int scopeDepth;
 } Compiler;
 
 Parser parser;
-Compiler *current;
+Compiler *current = NULL;
 Chunk *compilingChunk;
 
+// The current chunk is always the chunk
+// owned by the function we're in the middle of compiling
 static Chunk *currentChunk()
 {
-  return compilingChunk;
+  return &current->function->chunk;
 }
 
 static void errorAt(Token *token, const char *message)
@@ -169,6 +180,8 @@ static int emitJump(uint8_t instruction)
 
 static void emitReturn()
 {
+  // default return value (for implicit return)
+  emitByte(OP_NIL);
   // store the bytecode for `return` token
   emitByte(OP_RETURN);
 }
@@ -204,20 +217,39 @@ static void patchJump(int offset)
   currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
-static void initCompiler(Compiler *compiler)
+static void initCompiler(Compiler *compiler, FunctionType type)
 {
+  compiler->enclosing = current;
+  compiler->function = NULL;
+  compiler->type = type;
   compiler->localCount = 0;
   compiler->scopeDepth = 0;
+  compiler->function = newFunction();
   current = compiler;
+
+  if (type != TYPE_SCRIPT)
+    current->function->name = copyString(parser.previous.start,
+                                         parser.previous.length);
+
+  Local *local = &current->locals[current->localCount++];
+  local->depth = 0;
+  local->name.start = "";
 }
 
-static void endCompiler()
+static ObjFunction *endCompiler()
 {
   emitReturn();
+  ObjFunction *function = current->function;
+
 #ifdef DEBUG_PRINT_CODE
   if (!parser.hadError)
-    disassembleChunk(currentChunk(), "code");
+    disassembleChunk(
+        currentChunk(),
+        function->name != NULL ? function->name->chars : "<script>");
 #endif
+
+  current = current->enclosing;
+  return function;
 }
 
 static void beginScope()
@@ -284,6 +316,12 @@ static void binary(bool canAssign)
   default:
     return; // Unreachable;
   }
+}
+
+static void call(bool canAssign)
+{
+  uint8_t argCount = argumentList();
+  emitBytes(OP_CALL, argCount);
 }
 
 static void literal(bool canAssign)
@@ -395,7 +433,7 @@ static void unary(bool canAssign)
 // What the heck is this technique?
 // Google "Pratt parser" or "vaughan pratt top down operator precedence parsing"
 ParseRule rules[] = {
-    [TOKEN_LEFT_PAREN] = {grouping, NULL, PREC_NONE},
+    [TOKEN_LEFT_PAREN] = {grouping, NULL, PREC_CALL},
     [TOKEN_RIGHT_PAREN] = {NULL, NULL, PREC_NONE},
     [TOKEN_LEFT_BRACE] = {NULL, NULL, PREC_NONE},
     [TOKEN_RIGHT_BRACE] = {NULL, NULL, PREC_NONE},
@@ -551,7 +589,8 @@ static uint8_t parseVariable(const char *errorMessage)
 
 static void markInitialized()
 {
-
+  if (current->scopeDepth == 0)
+    return;
   current->locals[current->localCount - 1].depth = current->scopeDepth;
 }
 
@@ -564,6 +603,23 @@ static void defineVariable(uint8_t global)
     return;
   }
   emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
+static uint8_t argumentList()
+{
+  uint8_t argCount = 0;
+  if (!check(TOKEN_RIGHT_PAREN))
+  {
+    do
+    {
+      expression();
+      if (argCount == 255)
+        error("Can't have more than 255 arguments.");
+      argCount++;
+    } while (match(TOKEN_COMMA));
+  }
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after arguments.");
+  return argCount;
 }
 
 static void and_(bool canAssign)
@@ -584,6 +640,43 @@ static void block()
   while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF))
     declaration();
   consume(TOKEN_RIGHT_BRACE, "Expect '}'after block");
+}
+
+static void function(FunctionType type)
+{
+  Compiler compiler;
+  initCompiler(&compiler, type);
+  beginScope();
+
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+
+  if (!check(TOKEN_RIGHT_PAREN))
+  {
+    do
+    {
+      current->function->arity++;
+      if (current->function->arity > 255)
+        errorAtCurrent("Can't have more than 255 parameters.");
+      uint8_t constant = parseVariable("Expect variable name.");
+      defineVariable(constant);
+    } while (match(TOKEN_COMMA));
+  }
+
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+  consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
+  block();
+
+  ObjFunction *function = endCompiler();
+  emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+  // no endScope() needed because we end Compiler when we reach the end of the body
+}
+
+static void funDeclaration()
+{
+  uint8_t global = parseVariable("Expect function name.");
+  markInitialized();
+  function(TYPE_FUNCTION);
+  defineVariable(global);
 }
 
 static void varDeclaration()
@@ -686,6 +779,20 @@ static void printStatement()
   emitByte(OP_PRINT);
 }
 
+static void returnStatement()
+{
+  if (current->type == TYPE_SCRIPT)
+    error("Cannot return from top-level code.");
+  if (match(TOKEN_SEMICOLON))
+    emitReturn();
+  else
+  {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after return value");
+    emitByte(OP_RETURN);
+  }
+}
+
 static void whileStatement()
 {
   int loopStart = currentChunk()->count;
@@ -729,7 +836,9 @@ static void synchronize()
 
 static void declaration()
 {
-  if (match(TOKEN_VAR))
+  if (match(TOKEN_FUN))
+    funDeclaration();
+  else if (match(TOKEN_VAR))
     varDeclaration();
   else
     statement();
@@ -746,6 +855,8 @@ static void statement()
     forStatement();
   else if (match(TOKEN_IF))
     ifStatement();
+  else if (match(TOKEN_RETURN))
+    returnStatement();
   else if (match(TOKEN_WHILE))
     whileStatement();
   else if (match(TOKEN_LEFT_BRACE))
@@ -758,12 +869,11 @@ static void statement()
     expressionStatement();
 }
 
-bool compile(const char *source, Chunk *chunk)
+ObjFunction *compile(const char *source)
 {
   initScanner(source);
   Compiler compiler;
-  initCompiler(&compiler);
-  compilingChunk = chunk;
+  initCompiler(&compiler, TYPE_SCRIPT);
 
   parser.hadError = false;
   parser.panicMode = false;
@@ -775,6 +885,6 @@ bool compile(const char *source, Chunk *chunk)
     declaration();
   }
 
-  endCompiler();
-  return !parser.hadError;
+  ObjFunction *function = endCompiler();
+  return parser.hadError ? NULL : function;
 }
--- vm.h
+++ vm.h
@@ -1,16 +1,27 @@
 #ifndef clox_vm_h
 #define clox_vm_h
 
-#include "chunk.h"
+#include "object.h"
 #include "table.h"
 #include "value.h"
 
-#define STACK_MAX 256
+#define FRAMES_MAX 64
+#define STACK_MAX (UINT8_COUNT * FRAMES_MAX)
 
 typedef struct
 {
-  Chunk *chunk;
+  ObjFunction *function;
+  // the caller stores its own instruction pointer
   uint8_t *ip;
+  // points into the VM's value stack at the first slot this func can use
+  Value *slots;
+} CallFrame;
+
+typedef struct
+{
+  CallFrame frames[FRAMES_MAX];
+  int frameCount;
+
   Value stack[STACK_MAX];
   Value *stackTop;
   Table globals;
